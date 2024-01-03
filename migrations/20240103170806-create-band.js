0cb4fb466ed201d2870c01f6dc51282971c22321
NeNeZoRr <zorrillalin@gmail.com>
2024-01-03 12:12:48 -0500

    update4

---
+++ migrations/20240103170806-create-band.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bands', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      band_id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      genre: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      available_start_time: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      end_time: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bands');
+  }
+};
